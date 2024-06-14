9ef4684341bb1ffd93ca4468c64bb77f147bce4a
Omar A. H. Abo El Wafa <58788836+omarAboElWafa@users.noreply.github.com>
2024-06-15 01:15:45 +0300

    feat: Initiate the server instance and db (w/ separation of concerns)

--- src/server.ts
+++ src/server.ts
@@ -3,17 +3,17 @@ import cors from "cors";
 import routes from "./loaders/v1/routes";
 import { PORT } from "./config/env";
 
-async function startServer(): Promise<void> {
+export const createServer = async (): Promise<Express> => {
   const app: Express = express();
   app.use(cors());
   app.use(express.json());
   routes(app);
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
-}
 
-startServer().catch((error) => {
-  console.error("Failed to start the server", error);
-  process.exit(1);
-});
+  return app;
+};
+
+export const startServer = (app: Express): void => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+  });
+};
