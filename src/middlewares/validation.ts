35dd508fdfd29ef402fe493e05b3a47f58730b94
Omar A. H. Abo El Wafa <58788836+omarAboElWafa@users.noreply.github.com>
2024-06-15 14:58:16 +0300

    feat: Add schemas for checking params and tenant payloads

--- src/middlewares/validation.ts
+++ src/middlewares/validation.ts
@@ -15,11 +15,35 @@ export function validateData(
         const errorMessages = error.errors.map((issue) => ({
           message: `${issue.path.join(".")} is ${issue.message}`,
         }));
-        res
+        return res
           .status(StatusCodes.BAD_REQUEST)
           .json({ error: "Invalid data", details: errorMessages });
       } else {
-        res
+        return res
+          .status(StatusCodes.INTERNAL_SERVER_ERROR)
+          .json({ error: "Internal Server Error" });
+      }
+    }
+  };
+}
+
+export function validateParams(
+  schema: z.ZodObject<ZodRawShape, UnknownKeysParam>,
+) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse({ id: parseInt(req.params.id) });
+      next();
+    } catch (error) {
+      if (error instanceof ZodError) {
+        const errorMessages = error.errors.map((issue) => ({
+          message: `${issue.path.join(".")} is ${issue.message}`,
+        }));
+        return res
+          .status(StatusCodes.BAD_REQUEST)
+          .json({ error: "Invalid data", details: errorMessages });
+      } else {
+        return res
           .status(StatusCodes.INTERNAL_SERVER_ERROR)
           .json({ error: "Internal Server Error" });
       }
